9b959482569fa86791d8f09a07d2ffbfb3c89e0b
FKANE <fg.kane@gmail.com>
2020-07-30 11:19:44 +0100

    Testing methods

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM.BL
 {
@@ -22,7 +23,7 @@ namespace ACM.BL
                     }
                     fullName += FirstName;
                 }
-                                
+
                 return fullName;
             }
         }
@@ -39,5 +40,47 @@ namespace ACM.BL
                 _lastName = value;
             }
         }
+
+
+        ///<summary>
+        ///Retrieve one customer.</summary>
+        ///<returns></returns>///
+        public Customer Retrieve(int CustomerId)
+        {
+            // code that defines retrieved customer
+            return new Customer();
+        }
+
+        ///<summary>Returns a list of all the customers</summary>
+        ///<returns></returns>
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        ///<summary>Saves the current customer</summary>
+        ///<returns></returns>
+        public bool Save()
+        {
+            // code that saves the defined customer
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the customer data.
+        /// </summary>
+        ///<returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+        public static int InstanceCount { get; set; }
     }
+
 }
